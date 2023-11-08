4f575fb030820def08b295a27b3f2dce4b6d89b1
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 17:25:50 -0500

    insert data for geology

--- src/app/components/geology.component.ts
+++ src/app/components/geology.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { PlanetComponent } from './planet.component';
 
 @Component({
   selector: 'app-geology',
-  template: ``,
-  styles: [``]
+  template: `
+  <div class="images">
+    <img [src]="planetComponent.planet.images.planet" alt="">
+    <img [src]="planetComponent.planet.images.geology" alt="">
+  </div>
+  <div>
+    <h2>{{ planetComponent.planet.name }}</h2>
+    <p>{{ planetComponent.planet.geology.content }}</p>
+    <p>Source: <a [href]="planetComponent.planet.geology.source">Wikipedia</a></p>
+  </div>
+  `,
+  styles: [`
+  .images {
+    position: relative;
+  }
+  .images > img:last-child {
+    position: absolute;
+  }
+  `]
 })
 export class GeologyComponent {
-
+  constructor(public planetComponent: PlanetComponent) {}
 }
