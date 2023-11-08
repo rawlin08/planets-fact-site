e2f4e5c994aabb7613ee52bad6ec57df5b275a1c
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 17:00:16 -0500

    import data, grab data based on url

--- src/app/components/planet.component.ts
+++ src/app/components/planet.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import * as planetData from 'src/assets/data.json';
 
 @Component({
   selector: 'app-planet',
   template: `
-  
+  <header>
+    <app-heading></app-heading>
+  </header>
   `,
-  styles: [``]
+  styles: [`
+  header {
+    padding: 20px;
+    border-bottom: 2px solid var(--dark-gray);
+  }
+  `]
 })
 export class PlanetComponent implements OnInit {
   constructor(public route: ActivatedRoute, public router: Router) {}
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
-      let uid:any = params.get('planet');
+      this.uid = params.get('planet');
+      let capitalized = this.uid.charAt(0).toUpperCase() + this.uid.slice(1);
+      this.data = planetData;
+      this.planet = this.data[0].find((element:any) => element.name == capitalized);
+      console.log(this.planet);
     });
   }
+  uid:any;
+  data:any;
+  planet:any;
 }
