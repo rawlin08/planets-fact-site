b7799f2aa7e6841daa4e73ddec3a86210e59076b
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 16:59:27 -0500

    routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlanetComponent } from './components/planet.component';
+import { OverviewComponent } from './components/overview.component';
+import { InternalComponent } from './components/internal.component';
+import { GeologyComponent } from './components/geology.component';
 
 const routes: Routes = [
-  { path: ':planet', title: ':planet', component: PlanetComponent }
+  { path: 'planets/:planet', title: 'Planets', component: PlanetComponent, children: [
+    {
+      path: 'overview',
+      component: OverviewComponent,
+    },
+    {
+      path: 'internal',
+      component: InternalComponent,
+    },
+    {
+      path: 'geology',
+      component: GeologyComponent,
+    },
+  ] }
 ];
 
 @NgModule({
