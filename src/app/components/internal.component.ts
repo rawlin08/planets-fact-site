4a1c8da1d38eafcbf88fb27264fd265643ff05c1
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 17:26:06 -0500

    insert data for structure of planet

--- src/app/components/internal.component.ts
+++ src/app/components/internal.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { PlanetComponent } from './planet.component';
 
 @Component({
   selector: 'app-internal',
-  template: ``,
+  template: `
+  <img [src]="planetComponent.planet.images.internal" alt="">
+  <div>
+    <h2>{{ planetComponent.planet.name }}</h2>
+    <p>{{ planetComponent.planet.structure.content }}</p>
+    <p>Source: <a [href]="planetComponent.planet.structure.source">Wikipedia</a></p>
+  </div>
+  `,
   styles: [``]
 })
 export class InternalComponent {
-
+  constructor(public planetComponent: PlanetComponent) {}
 }
