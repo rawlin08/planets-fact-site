ba437eb874f11bedd75fb4c560127540b3031251
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 18:00:44 -0500

    styles for component

--- src/app/components/overview.component.ts
+++ src/app/components/overview.component.ts
@@ -11,7 +11,39 @@ import { PlanetComponent } from './planet.component';
     <p>Source: <a [href]="planetComponent.planet.overview.source">Wikipedia</a></p>
   </div>
   `,
-  styles: [``]
+  styles: [`
+  h2, p {
+    text-align: center;
+  }
+  h2 {
+    font-family: 'Antonio', sans-serif;
+    font-size: 40px;
+    font-weight: 400;
+    text-transform: uppercase;
+  }
+  p {
+    font-family: 'League Spartan', sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+  }
+  div > p:nth-child(2) {
+    margin: 16px 0 32px 0;
+  }
+  div > p:last-child {
+    font-size: 12px;
+    opacity: 0.5;
+  }
+  a {
+    text-decoration: underline;
+    font-weight: 700;
+  }
+  img {
+    width: 111px;
+    height: auto;
+    margin: 100px 0;
+  }
+  `]
 })
 export class OverviewComponent {
   constructor(public planetComponent: PlanetComponent) {}
