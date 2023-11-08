25096a5189ff5500e1332ef85b7ef4b6edf1c5e1
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 18:57:25 -0500

    h1 styles

--- src/app/components/heading.component.ts
+++ src/app/components/heading.component.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-heading',
   template: `
   <h1>THE PLANETS</h1>
+  <app-menu></app-menu>
   `,
-  styles: [``]
+  styles: [`
+  h1 {
+    font-family: 'Antonio', sans-serif;
+    font-size: 28px;
+    letter-spacing: -1.05px;
+    font-weight: 400;
+  }
+  `]
 })
 export class HeadingComponent {
 
