7fe87887eb6ee8987d38f3c2efb2424a1d1836cb
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 17:00:38 -0500

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanetComponent } from './components/planet.component';
+import { HeadingComponent } from './components/heading.component';
+import { MenuComponent } from './components/menu.component';
+import { OverviewComponent } from './components/overview.component';
+import { InternalComponent } from './components/internal.component';
+import { GeologyComponent } from './components/geology.component';
+import { OptionsComponent } from './components/options.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlanetComponent
+    PlanetComponent,
+    HeadingComponent,
+    MenuComponent,
+    OverviewComponent,
+    InternalComponent,
+    GeologyComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
