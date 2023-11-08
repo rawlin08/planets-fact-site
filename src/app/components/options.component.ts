678f35e6f7ba0b6f34e079024b7d7743a7de816b
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 18:57:36 -0500

    links and styles for the options

--- src/app/components/options.component.ts
+++ src/app/components/options.component.ts
@@ -3,9 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-options',
   template: `
-  
+  <a routerLink="./overview" routerLinkActive="activeLink">OVERVIEW</a>
+  <a routerLink="./internal" routerLinkActive="activeLink">STRUCTURE</a>
+  <a routerLink="./geology" routerLinkActive="activeLink">SURFACE</a>
   `,
-  styles: [``]
+  styles: [`
+  a {
+    font-family: 'League Spartan', sans-serif;
+    font-size: 12px;
+    font-weight: 700;
+    letter-spacing: 2px;
+    opacity: 0.5;
+    padding: 20px 0;
+  }
+  .activeLink {
+    border-bottom: 3px solid var(--light-blue);
+    opacity: 1;
+  }
+  `]
 })
 export class OptionsComponent {
 
