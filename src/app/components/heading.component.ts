b07f426a372345aa943d6ff1c135ac2b9f7c26f1
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 16:59:53 -0500

    new component

---
+++ src/app/components/heading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  template: `
+  <h1>THE PLANETS</h1>
+  `,
+  styles: [``]
+})
+export class HeadingComponent {
+
+}
