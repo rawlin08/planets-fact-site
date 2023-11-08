bd763b3259cca44ebe1f09c75b5afc2e48484e3a
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 17:26:20 -0500

    insert data for overview of planet

--- src/app/components/overview.component.ts
+++ src/app/components/overview.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { PlanetComponent } from './planet.component';
 
 @Component({
   selector: 'app-overview',
   template: `
-  
+  <img [src]="planetComponent.planet.images.planet" alt="">
+  <div>
+    <h2>{{ planetComponent.planet.name }}</h2>
+    <p>{{ planetComponent.planet.overview.content }}</p>
+    <p>Source: <a [href]="planetComponent.planet.overview.source">Wikipedia</a></p>
+  </div>
   `,
   styles: [``]
 })
 export class OverviewComponent {
-
+  constructor(public planetComponent: PlanetComponent) {}
 }
