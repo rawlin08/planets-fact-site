e0b6c39295b0372b02a6fed97a74d9c4d790ec10
Alec Rawlins <alec12999@yahoo.com>
2023-11-07 22:58:29 -0500

    routing for planet selection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlanetComponent } from './components/planet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ':planet', title: ':planet', component: PlanetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
