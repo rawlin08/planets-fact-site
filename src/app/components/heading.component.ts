d4ad749979900d82b413b2f405770a0fbdf39e2f
Alec Rawlins <alec12999@yahoo.com>
2023-11-08 21:00:52 -0500

    toggle menu

--- src/app/components/heading.component.ts
+++ src/app/components/heading.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PlanetComponent } from './planet.component';
 
 @Component({
   selector: 'app-heading',
   template: `
   <h1>THE PLANETS</h1>
-  <img src="assets/images/icons/icon-hamburger.svg" alt="">
+  <img [className]="planetComponent.menuOpened ? 'active' : ''" (click)="planetComponent.toggleMenu()" src="assets/images/icons/icon-hamburger.svg" alt="">
   `,
   styles: [`
   h1 {
@@ -13,8 +14,14 @@ import { Component } from '@angular/core';
     letter-spacing: -1.05px;
     font-weight: 400;
   }
+  img {
+    cursor: pointer;
+  }
+  .active {
+    opacity: 0.5;
+  }
   `]
 })
 export class HeadingComponent {
-
+  constructor(public planetComponent: PlanetComponent) {}
 }
