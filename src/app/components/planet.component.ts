347b6109fa9814f196f22c38b25be0b604dbb588
Alec Rawlins <alec12999@yahoo.com>
2023-11-10 23:50:48 -0500

    redirect to first planet if url is mistyped

--- src/app/components/planet.component.ts
+++ src/app/components/planet.component.ts
@@ -12,8 +12,8 @@ import * as planetData from 'src/assets/data.json';
   <main>
     <div *ngIf="menuOpened == false">
       <app-options></app-options>
-      <router-outlet></router-outlet>
-      <app-bottom-tabs></app-bottom-tabs>
+      <router-outlet *ngIf="this.planet"></router-outlet>
+      <app-bottom-tabs *ngIf="this.planet"></app-bottom-tabs>
     </div>
   </main>
   `,
@@ -23,12 +23,14 @@ export class PlanetComponent implements OnInit {
   constructor(public route: ActivatedRoute, public router: Router) {}
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
-      this.uid = params.get('planet');
+      this.uid = params.get('planet');      
       this.uid = this.uid.toLowerCase();
       let capitalized = this.uid.charAt(0).toUpperCase() + this.uid.slice(1);
       this.data = planetData;
       this.planet = this.data[0].find((element:any) => element.name == capitalized);
-      console.log(this.planet);
+      if (!this.planet) {
+        this.router.navigate([`/planets/mercury/overview`]);
+      }
     });
   }
   uid:any;
